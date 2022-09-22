53abd10faaa02e1e3fd2acfe42fc12edb7bafc2d
zhangjing15 <zhangjing@hozonauto.com>
2022-09-22 14:07:24 +0800

    release Hozon_Token

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 #git pull
 
 # 配置Hozon-UED分支
-git config -f .gitmodules submodule.Hozon-UED.branch main
+git config -f .gitmodules submodule.Hozon_Tokens.branch main
 # 更新Hozon-UED
 git submodule update --remote
 
-token-transformer Hozon-UED/tokens.json token_out.json
+token-transformer Hozon_Tokens/8675_dark_tokens.json token_out.json
 
 #mv Hozon-UED/tokens.json Hozon-UED/tokens.json.bak
 
